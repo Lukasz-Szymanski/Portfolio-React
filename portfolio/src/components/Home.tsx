8960e254afb936f48c3fb9d1e025c9f0d7331390
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2025-01-11 21:48:10 +0100

    Refactor Home component: Update layout to include a full-screen div with centered title, enhancing visual appeal and content relevance.

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <h1 className="fixed z-10" id="home">
-      Home Component
-    </h1>
+    <div
+      className="fixed z-10 w-full h-screen flex items-center justify-center body-font pb-64 lg:pb-40"
+      id="home">
+      <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-center">
+        Muzyk z pasją programowania
+      </h1>
+    </div>
   );
 };
 
